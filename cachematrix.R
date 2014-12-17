2fc33618d6bc8f520698a4ff4093547c95fb8eec
Klas Hermodsson <klas.hermodsson@sonymobile.com>
2014-12-17 10:37:49 +0100

    Implemented and commented the functions makeCacheMatrix and cacheSolve.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions makeCacheMatrix and cacheSolve
+## The functions calculates the inverse of a matrix but with the
+## added benefit that the result is cached and will not be recalculated
+## if it has already been solved before.
 
-## Write a short comment describing this function
+## makeCacheMatrix
+## This function caches the matrix and the result of the inversematrix operation.
+## The function has a set of internal functions that help cache the matrix
+## and the inverse matrix.
+## If the matrix changes the cached result will also be deleted.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # first reset the inv matrix cache
+  invmtrx <- NULL
+  ## set - caches a new matrix and resets the inv matrix cache
+  set <- function(y) {
+    x <<- y
+    invmtrx <<- NULL
+  }
+  ## get - returns the matrix itself
+  get <- function() x
+  ## setinvmatrix - store a solution to the inverse matrix
+  setinvmtrx <- function(inv) invmtrx <<- inv
+  ## getinvmatrix - retrieves the cached inv matrix
+  getinvmtrx <- function() invmtrx
+  list(set = set, get = get,
+       setinvmtrx = setinvmtrx,
+       getinvmtrx = getinvmtrx)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve
+## Returns a matrix that is the inverse of 'x' and caches the result.
+## If a cahed solution already exists this will be retrieved and returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # Check if a cached solution already exists
+  invmtrx <- x$getinvmtrx()
+  if (!is.null(invmtrx)) {
+    message("getting cached solution")
+    return(invmtrx)
+  }
+  # A cached solution does not exist, therefore calculate the inverse matrix
+  # and cached it.
+  data <- x$get()
+  invmtrx <- solve(data, ...)
+  x$setinvmtrx(invmtrx)
+  invmtrx
 }
